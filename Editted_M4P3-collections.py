dc35a4574ecac7d901d692952da32152addf8dd1
angelineh64 <60159090+angelineh64@users.noreply.github.com>
2020-02-11 13:19:01 -0600

    Add files via upload

---
+++ Editted_M4P3-collections.py
@@ -0,0 +1,21 @@
+# Create a collection of these authors and
+# the year they kicked the bucket;
+# print the collection in the following format:
+
+# Charles Dickens died in 1870.
+
+# Charles Dickens, 1870
+# William Thackeray, 1863
+# Anthony Trollope, 1882
+# Gerard Manley Hopkins, 1889
+
+authors = {
+    "Charles Dickens": "1863",
+    "William Thackeray": "1870",
+    "Anthony Trollope": "1889",
+    "Gerard Manley Hopkins": "1882"}
+
+# Was originally 'author.items'. Needed to be 'authors.items'
+# 'authors' is defined. 'author' is not.
+for authors, date in authors.items():
+    print("%s" % authors, " died in ", "%s." % date)
