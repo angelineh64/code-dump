7944d47d8ab322c681f284dbd86aa190e54b7d30
angelineh64 <60159090+angelineh64@users.noreply.github.com>
2020-02-11 13:25:41 -0600

    Update Editted_M4P1-time.py

--- Editted_M4P1-time.py
+++ Editted_M4P1-time.py
@@ -1,8 +1,14 @@
+Last editted by: Angeline Hidalgo
+Editted on: Februrary 9, 2020
+
+# Prompts user to enter their current time in hours.
 currentTimeStr = input("What is the current time (in hours 0-23)?")
+# Prompts user to enter how many hours they want to wait.
 waitTimeStr = input("How many hours do you want to wait?")
 
 currentTimeInt = int(currentTimeStr)
 waitTimeInt = int(waitTimeStr)
 
+# Prints out the expected final time with the equation provided for 'finalTimeInt'.
 finalTimeInt = currentTimeInt + waitTimeInt
 print(finalTimeInt)
