fc4cd52f4d08f7cdb53c07cdb132ad3f2f959d49
angelineh64 <60159090+angelineh64@users.noreply.github.com>
2020-02-11 13:26:54 -0600

    Update Editted_M4P5-grading.py

--- Editted_M4P5-grading.py
+++ Editted_M4P5-grading.py
@@ -1,3 +1,6 @@
+Last editted by: Angeline Hidalgo
+Editted on: Februrary 9, 2020
+    
 # Calculating Grades (ok, let me think about this one)
 
 # Write a program that will average 3 numeric exam grades, return an average test score, a corresponding letter grade, and a message stating whether the student is passing.
