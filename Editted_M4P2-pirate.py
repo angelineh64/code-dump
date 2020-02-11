dc99ee41047142b7d14ab8cf9d2b2e3b638d8084
angelineh64 <60159090+angelineh64@users.noreply.github.com>
2020-02-11 13:28:16 -0600

    Update Editted_M4P2-pirate.py

--- Editted_M4P2-pirate.py
+++ Editted_M4P2-pirate.py
@@ -1,4 +1,9 @@
+Last editted by: Angeline Hidalgo
+Editted on: Februrary 9, 2020
+
+# Prompts user to enter a greeting to discern if they are a pirate.
 greeting = input("Hello, possible pirate! What's the password?")
+# Needed "==" in order to process the 'if' statement.
 if greeting == ("Arrr!"):
     print("Go away, pirate.")
 else:
